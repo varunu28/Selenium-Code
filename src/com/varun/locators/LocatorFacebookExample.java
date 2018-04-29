fa46eab792e1d638577232c425dc5923dba8a4d5
Varun Upadhyay <varun.u28@gmail.com>
2018-04-29 15:58:29 -0700

    Added code for locator examples

---
+++ src/com/varun/locators/LocatorFacebookExample.java
@@ -0,0 +1,26 @@
+package com.varun.locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LocatorFacebookExample {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "/home/varunu28/Downloads/Softwares/chromedriver");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		// Opening a browser for Facebook
+		driver.get("https://facebook.com");
+		
+		// Passing a text to the element with "email" id
+		driver.findElement(By.id("email")).sendKeys("abc@xyz.com");
+		
+		// Passing a text to the element with "password" name attribute
+		driver.findElement(By.name("pass")).sendKeys("password");
+		
+		// Locating an element using the linked text
+		driver.findElement(By.linkText("Forgot account?")).click();
+	}
+}
---
+++ src/com/varun/locators/LocatorSalesforceExample.java
@@ -0,0 +1,23 @@
+package com.varun.locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LocatorSalesforceExample {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "/home/varunu28/Downloads/Softwares/chromedriver");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		// Opening a browser for Salesforce Login
+		driver.get("https://login.salesforce.com");
+		
+		// Passing a text to the element with "username" id
+		driver.findElement(By.id("username")).sendKeys("hello");
+		
+		// Passing a text to the element with "pw" name attribute
+		driver.findElement(By.name("pw")).sendKeys("password");
+	}
+}
